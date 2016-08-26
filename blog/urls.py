f7bbcb2f073fcf38c541c37ab667769ac9980518
Sanghee <sanghee911@hotmail.com>
2016-08-26 14:43:51 +0900

    changed blog.urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from .views import PostListView, PostDetailView, CategoryDetailView, TagDetailVi
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='post_list'),
-    url(r'^posts/$', PostListView.as_view(), name='post_list'),
+    # url(r'^posts/$', PostListView.as_view(), name='post_list'),
     url(r'^posts/(?P<slug>[\w-]+)/$', PostDetailView.as_view(), name='post_detail'),
     url(r'^posts/category/(?P<slug>[\w-]+)/$', CategoryDetailView.as_view(), name='category_detail'),
     url(r'^posts/tag/(?P<slug>[\w-]+)/$', TagDetailView.as_view(), name='tag_detail'),
