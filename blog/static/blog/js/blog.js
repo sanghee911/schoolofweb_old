de1c033406f70700bb4bec2e8d79ba452508c82c
Sanghee <sanghee911@hotmail.com>
2016-09-08 23:06:48 +0900

    changed sidebar color scheme

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -12,11 +12,11 @@ $(document).ready(function () {
 });
 
 // this script keeps sidebar height same as content height
-$(window).on('load resize', function () {
-    if (window.matchMedia('(min-width: 992px)').matches) {
-        var docHeight = $(document).height();
-        $('.sidebar').height(docHeight);
-    } else {
-        $('.sidebar').height('auto');
-    }
-});
+// $(window).on('load resize', function () {
+//     if (window.matchMedia('(min-width: 992px)').matches) {
+//         var docHeight = $(document).height();
+//         $('.sidebar').height(docHeight);
+//     } else {
+//         $('.sidebar').height('auto');
+//     }
+// });
